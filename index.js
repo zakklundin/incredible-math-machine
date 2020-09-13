187120a3da414db1be4e9627ebf922d673326105
Zakk Lundin <zakk.lundin@skola.botkyrka.se>
2020-09-13 17:06:56 +0200

    Mission complete

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const element = document.getElementById('math')
 
 // Ask user for numbers
-prompt('Give me a number'
-prompt('Give me another number'
+let a = prompt('Give me a number')
+let b = prompt('Give me another number')
+let numberOne = parseInt(a)
+let numberTwo = parseInt(b)
 
 // TODO: log the input numbers to the console!
 
-sum
+let sum = numberOne + numberTwo
+console.log(sum)
 // TODO: log the sum to the console!
 
 // Template literal string
-element.innerHTML = `${a} + b = {sum}`
+element.innerHTML = `${a} + ${b} = ${sum}`
